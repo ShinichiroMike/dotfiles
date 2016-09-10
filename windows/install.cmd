61626d2f12d0507192c9181a0ef4a8bc80f01746
shinichiroMike <miguelo_0000@hotmail.com>
2016-09-10 14:48:05 +0200

    win install

---
+++ windows/install.cmd
@@ -0,0 +1,20 @@
+ECHO Installing packages with chocolatey
+choco install chocolateygui
+choco install googlechrome
+choco install 7zip.install
+choco install winrar
+choco install ccleaner
+choco install vlc
+choco install atom
+choco install strokesplus
+choco install babun
+choco install dropbox
+choco install itunes
+choco install github
+choco install steam
+choco install spotify
+choco install utorrent
+
+// TODO: install packages for babun : pact
+// TODO: install atom packages : apm
+// TODO: configure dotfiles inside babun environment, also with stow
